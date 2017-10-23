aa5c72fa0dc983dcc77ffbd341f4fc959d65da5e
Sonchellik <Sonchellik@users.noreply.github.com>
2017-10-23 19:39:43 +0200

    Add files via upload

---
+++ includes/pie.php
@@ -0,0 +1,32 @@
+</div><!--FIN WRAPPER-->
+
+<div id="footer" class="navbar-fixed-bottom">
+
+<div id="pajaritos">
+<img src="assets/img/Pajaritos.png" class="img-responsive">
+</div>
+
+<!-- Modal Bodies come here -->
+
+<!--   end modal -->
+<div class="panel-footer">
+	<p class="text-center">COPYRIGHT <i class="glyphicon glyphicon-copyright-mark"></i> 2017 Gallega de Ornitología, S.L. | Contacto: <a href="mailto:gallegadeornitologia@gmail.com">gallegadeornitologia@gmail.com <span class="glyphicon glyphicon-envelope"></span></a> | <br><i class="glyphicon glyphicon-earphone"></i> 604 05 43 06 | Aviso Legal <a href="avisolegal.php" target="_new"><i class="glyphicon glyphicon-info-sign"></i></a></p>
+</div>
+
+</div><!--FIN FOOTER-->
+
+</div><!--FIN CAJA-->
+</body>
+
+    <script src="assets/js/jquery-1.10.2.js" type="text/javascript"></script>
+	<script src="assets/js/jquery-ui-1.10.4.custom.min.js" type="text/javascript"></script>
+    
+	<!--  Plugins -->
+	<script src="assets/js/ct-paper-checkbox.js"></script>
+	<script src="assets/js/ct-paper-radio.js"></script>
+	<script src="assets/js/bootstrap-select.js"></script>
+	<script src="assets/js/bootstrap-datepicker.js"></script>
+
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
+	   
+</html>
